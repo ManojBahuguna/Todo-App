be55ec7bd17beed1c708aa925060bbad3bd69f8a
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-22 14:13:41 +0530

    Initial Setup

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div className="TodoApp">
+        TodoApp
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
