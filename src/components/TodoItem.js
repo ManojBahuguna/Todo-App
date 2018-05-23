5650b86e063f5189b1c16c37707b0c20c2a73ab6
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-23 12:39:06 +0530

    Handle todo toggle 'isDone'

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { toggleTodo } from '../actions';
 import Icon from '@fortawesome/react-fontawesome';
 import {
   faCheckCircle as checkIcon,
   faTrashAlt as removeIcon
 } from '@fortawesome/fontawesome-free-regular';
 
-const TodoItem = ({ todo, dispatch }) => (
-  <li className="TodoItem">
-    <button className="TodoDoneCheckbox">
-      <Icon icon={checkIcon} />
-    </button>
+class TodoItem extends Component {
 
-    <p className="TodoText">{todo.text}</p>
+  handleTodoToggle = () => {
+    const id = this.props.todo.id;
+    this.props.dispatch(toggleTodo(id));
+  };
 
-    <button className="TodoRemoveButton">
-      <Icon icon={removeIcon} />
-    </button>
-  </li>
-);
+  render() {
+    const { text, isDone } = this.props.todo;
+    return (
+      <li className={`TodoItem ${isDone ? 'isDone' : ''}`}>
+        <button onClick={this.handleTodoToggle} className="TodoDoneCheckbox">
+          <Icon icon={checkIcon} />
+        </button>
+
+        <p className="TodoText">{text}</p>
+
+        <button className="TodoRemoveButton">
+          <Icon icon={removeIcon} />
+        </button>
+      </li>
+    );
+  }
+}
 
 export default connect()(TodoItem);
