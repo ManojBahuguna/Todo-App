d84bc933678ffb8243c36fbe447e6163cc8a84af
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-05-23 11:26:52 +0530

    handle state in 'AddNewTodoForm' component

--- src/components/AddNewTodoForm.js
+++ src/components/AddNewTodoForm.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Icon from '@fortawesome/react-fontawesome';
 import saveIcon from '@fortawesome/fontawesome-free-regular/faSave';
+import { addTodo } from '../actions/index';
 
-const AddNewTodoForm = () => {
-  return (
-    <form className="AddNewTodoForm">
-      <input type="text" />
-      <button><Icon icon={saveIcon} /></button>
-    </form >
-  );
-};
+class AddNewTodoForm extends Component {
+
+  state = {
+    input: ''
+  };
+
+  handleInput = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const text = this.state.input;
+    if (text.length === 0)
+      return;
+
+    this.props.dispatch(addTodo(text));
+    this.setState({
+      input: ''
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="AddNewTodoForm">
+        <input autoFocus value={this.state.input} onInput={this.handleInput} type="text" />
+        <button><Icon icon={saveIcon} /></button>
+      </form >
+    );
+  }
+
+}
 
 export default connect()(AddNewTodoForm);
